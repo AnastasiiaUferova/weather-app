31452660c5351ed4994172ef420eccde776d50e8
‘Anastasiia <miss.iuferova@yandex.ru>
2023-02-11 15:47:52 +0100

    add images

--- src/utilities/Utilities.ts
+++ src/utilities/Utilities.ts
@@ -1,6 +1,32 @@
-import BrokenClouds from "../images/Broken-clouds.svg"
+import BrokenClouds from "../images/Broken-clouds.svg";
+import ClearSkyNight from "../images/Clear-sky-night.svg";
+import ClearSkySun from "../images/Clear-sky-night.svg";
+import FewCloudsDay from "../images/Few-clouds-day.svg";
+import FewCloudsNight from "../images/Few-clouds-night.svg";
+import Mist from "../images/Mist.svg";
+import RainDay from "../images/Rain-day.svg";
+import RainNight from "../images/Rain-night.svg";
+import ScatteredCloudsDay from "../images/Scattered-clouds-day.svg";
+import ScatteredCloudsNight from "../images/Scattered-clouds-night.svg";
+import ShowerRainDay from "../images/Shower-rain-day.svg";
+import ShowerRainNight from "../images/Shower-rain-night.svg";
+import Snow from "../images/Snow.svg";
+import Thunderstorm from "../images/Thunderstorm.svg";
 
 
 export const Images = {
     "Broken-Clouds": BrokenClouds,
+    "Clear-sky-night": ClearSkyNight,
+    "Clear-sky-sun": ClearSkySun,
+    "Few-clouds-day": FewCloudsDay,
+    "Few-clouds-night": FewCloudsNight,
+    "Mist": Mist,
+    "Rain-day": RainDay,
+    "Rain-night":  RainNight,
+    "Scattered-clouds-day": ScatteredCloudsDay,
+    "Scattered-clouds-nigth": ScatteredCloudsNight,
+    "Shower-rain-day": ShowerRainDay,
+    "Shower-rain-night": ShowerRainNight,
+    "Snow": Snow,
+    "Thunderstorm": Thunderstorm
 }
