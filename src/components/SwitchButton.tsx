15eceb0baa2abf2b39d6b8aa53430122f399ec2a
‘Anastasiia <miss.iuferova@yandex.ru>
2023-02-13 17:44:18 +0100

    add switch button

--- src/components/SwitchButton.tsx
+++ src/components/SwitchButton.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import "../styles/SwitchButton/SwitchButton.css"
 
 export default function SwitchButton() {
   return (
-    <div>
-      <button>SwitchButton</button>
-    </div>
+  
+<div className="check-box">
+	<label className="label-C">C</label>
+	<label className="label-F">F</label>
+	<input type="checkbox" readOnly />
+</div>
   )
 }
