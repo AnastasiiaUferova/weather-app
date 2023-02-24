cb83852395e4af9eaaaa6d25403bd26d048d7122
‘Anastasiia <miss.iuferova@yandex.ru>
2023-02-24 12:19:14 +0100

    add useFetch

---
+++ src/api/useFetch.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function useFetch(url:string):(any) {
+  const [data, setData] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean | null>(false);
+  const [error, setError] = useState<any>(null);
+
+    useEffect(() => {
+      setLoading(true)
+      axios.get(url).then((res) => {
+        setData(res.data)
+      }).catch((err) => {
+        setError(err)
+      }).finally(() => setLoading(false))
+    }, [url])
+
+    const refetch = () => {
+      setLoading(true)
+      axios.get(url).then((res) => {
+        setData(res.data)
+      }).catch((err) => {
+        setError(err)
+      }).finally(() => setLoading(false))
+
+    }
+
+    return {data, loading, error, refetch}
+}
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,6 +6,7 @@ import Header from "./Header";
 import Main from "./Main";
 import Swiper from "./SwiperElement";
 import { useDebouncedCallback } from "use-debounce";
+import useFetch from "../api/useFetch";
 
 
 function App() {
@@ -28,12 +29,20 @@ function App() {
             window.removeEventListener('resize', Resize);
     } }, [Resize])
 
+    const {data, loading, error} = useFetch('https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=431e7a61c4a3556cbf4ffbf1a97345f3&units=metric')
+
+if(loading) return <h1>LOADING...</h1>
+if (error) console.log(error)
+console.log(data.coord.lon)
 
   return (
     <>
       <div className="App">
         <Header />
           {showMain ? <Main /> : <Swiper /> }
+      </div>
+      <div style={{width:"400px", height:"400px",background: "red", color: "white"}}>
+        {data?.coord.lon}
       </div>
     </>
   );
